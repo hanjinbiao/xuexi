9030e01aff909082066f2c644bf85350b109d7fd
root <root@room9pc01.tedu.cn>
2018-11-07 09:06:19 +0800

    shell

--- shell/myshell/1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-scp /root/lnmp_soft.tar.gz /root/lnmp.sh /root/nginx.sh root@$1:/root/
-
--- shell/myshell/background.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-num=$[RANDOM%5+1]
-
-/root/cw /root/tupian/${num}.jpg &>/dev/null
